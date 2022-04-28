a9993cc14cbf4999acf4309db8512d1e54eb7241
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-28 15:09:09 +0300

    This is both the exercises 2.2 - Course information step7

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Content = ({parts}) => {
-  const displayParts  = parts.map((part) => <p>{part.name} {part.exercises}</p>);
+const Content = ({parts, exercises}) => {
+  const displayParts  = parts.map((part) => <p key={part.id}>{part.name} {part.exercises}</p>);
+  const partsExercises = parts.map((part)=>part.exercises);
+  let sum = 0;
+  for(let exercise of partsExercises){
+    sum += exercise;
+  }
+  const totalExercises = sum;
+
   return (
     <div>
-      {displayParts} 
+      {displayParts}
+      All of the exercises {totalExercises}
     </div>
   );
 };
