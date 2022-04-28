43680bfc04c8e8cb9689f2f929034d327e48f876
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-04-28 15:13:05 +0300

    this is the exercise 2.3 - Course information step8 using the array method reduce

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 const Content = ({parts, exercises}) => {
   const displayParts  = parts.map((part) => <p key={part.id}>{part.name} {part.exercises}</p>);
   const partsExercises = parts.map((part)=>part.exercises);
-  let sum = 0;
-  for(let exercise of partsExercises){
-    sum += exercise;
-  }
-  const totalExercises = sum;
+  const totalExercises = partsExercises.reduce((acc, val)=>acc+val);
 
   return (
     <div>
