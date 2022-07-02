5b7796f99f0f44b933bc3ced1bf42e7876f1b8eb
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-07-02 15:48:49 +0300

    some refactoring

--- src/containers/persons/personForm.jsx
+++ src/containers/persons/personForm.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from "react";
-import Notifications from "../../components/Notifications";
-import { editPerson } from "../../services/Persons";
+import React, { useCallback, useEffect, useState } from "react";
+// import Error from "../../components/Error";
+import Alert from "../../components/Alert";
+import { createPerson, editPerson } from "../../services/Persons";
 
-const PersonForm = ({ onSave, defaultValues, persons, person, setPersons }) => {
+const PersonForm = ({ defaultValues, persons, person, setPersons }) => {
   const [values, setValues] = useState({
-    name: '',
-    number: '',
+    name: "",
+    number: ""
   });
 
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [alert, setAlert] = useState({
+    type: "",
+    message: ""
+  });
 
   useEffect(() => {
     if (defaultValues) {
       setValues((prev) => ({
         ...prev,
-        ...defaultValues,
+        ...defaultValues
       }));
     }
   }, [defaultValues]);
@@ -22,59 +26,137 @@ const PersonForm = ({ onSave, defaultValues, persons, person, setPersons }) => {
   const handleChange = (e) => {
     setValues((prev) => ({
       ...prev,
-      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value
     }));
   };
 
-  // update number
-  const onUpdatePerson = async () => {
-    const id = await persons.find((p) => p.name === values.name && p.id);
-
-    const newValues = { name: values.name, id: id.id, number: values.number };
-
-    const updatedPerson = await editPerson(id.id, newValues);
-
-    setPersons(
-      persons.map((person) =>
-        person.id === id.id ? { ...updatedPerson } : person
-      )
-    );
-  };
-
-  const onSubmit = async () => {
-    const timeOut = setTimeout(() => {
-      setErrorMessage(null);
-    }, 5000);
-    // if person exist
-    const samePersonName = persons.find(
-      (person) => person.name === values.name
-    );
-    if (samePersonName) {
-      // popup
-      const isOk = window.confirm(
-        `${values.name} is already added to phonebook, replace the old number with a new one?`
-      );
-      if (isOk) {
-        // update that person with the new values
-        onUpdatePerson(person.id);
-        setErrorMessage(`${values.name}'s number modified!`);
-        setValues(null);
-        timeOut();
+  const _handleCreatePerson = useCallback(
+    async (values) => {
+      try {
+        const person = await createPerson(values);
+        setPersons((prev) => {
+          return [
+            ...prev,
+            {
+              name:values.name,
+              number:values.number,
+              id: persons[persons.length - 1].id++,
+              ...person
+            }
+          ];
+        });
+      } catch (error) {
+        console.log(error.message);
       }
-      return;
+    },
+    [persons, setPersons]
+  );
+
+  // update number
+  const _onUpdatePerson = useCallback(async () => {
+    try {
+      const currentPerson = await persons.find(
+        (p) => p.name === values.name && p.id
+      );
+      console.log('currentPerson:', currentPerson);
+      
+
+      const newValues = {
+      ...currentPerson,
+       number: values.number
+      };
+      console.log('newValues:', newValues);
+      
+  
+      const updatedPerson = await editPerson(currentPerson.id, newValues);
+      console.log('updatedPerson:', updatedPerson);
+      
+      const newPersons = await persons.map((person) => {
+        if (person.id === currentPerson.id) {
+          return updatedPerson;
+        }
+        return person;
+      });
+      // const newPersons = persons.filter(p => p.name === values.name );
+  
+      setPersons(newPersons);
+    } catch (error) {
+      setPersons(persons.filter(p => p.name !== values.name ));
+      // console.log('error:', error.message);
+      setAlert({
+        type: "error",
+        message: `information of ${values.name} has been already removed from server`
+      });
+      setValues({
+        name: "",
+        number: ""
+      });
+      setTimeout(() => {
+        setAlert({ type: "" });
+      }, 5000);
     }
-    onSave(values);
-    setErrorMessage(`added ${values.name} ! `);
-    setValues('');
-    timeOut();
+    
+  }, [persons, setPersons, values]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // if person exist
+    try {
+      const samePersonName = persons.find(
+        (person) => person.name === values.name
+      );
+      if (samePersonName) {
+        // popup
+        const isOk = window.confirm(
+          `${values.name} is already added to phonebook, replace the old number with a new one?`
+        );
+        if (isOk) {
+          // update that person with the new values
+          await _onUpdatePerson(person.id);
+           
+          setAlert({
+            type: "success",
+            message: `${values.name}'s number modified!`
+          });
+          setValues({
+            name:"",
+            number:""
+          });
+          setTimeout(() => {
+            setAlert({ type: "" });
+          }, 5000);
+        }
+        return;
+      }
+      await _handleCreatePerson(values);
+      setAlert({
+        type: "success",
+        message: `${values.name} added! `
+      });
+      setValues({
+        name: "",
+        number: ""
+      });
+      setTimeout(() => setAlert({ type: "" }), 5000);
+    } catch (error) {
+        setAlert({
+          type: "error",
+          message: `information of ${values.name} has been already removed from server`
+        });
+        setValues({
+          name: "",
+          number: ""
+        });
+        setTimeout(() => {
+          setAlert({ type: "" });
+        }, 5000);
+    }
   };
 
   return (
     <div>
-      <Notifications message={errorMessage} values={values.name} />
-      <form
-        onSubmit={e => e.preventDefault()}
-      >
+      <Alert message={alert.message} type={alert.type} />
+      <form onSubmit={onSubmit}>
         <div>
           name:
           <input name="name" value={values.name} onChange={handleChange} />{" "}
@@ -85,10 +167,7 @@ const PersonForm = ({ onSave, defaultValues, persons, person, setPersons }) => {
           <input name="number" value={values.number} onChange={handleChange} />
         </div>
         <div>
-          <button type="submit" onClick={() => {onSubmit();
-          setValues('')}}>
-            Add
-          </button>
+          <button type="submit">Add</button>
         </div>
       </form>
     </div>
