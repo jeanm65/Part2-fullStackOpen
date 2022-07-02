9108cfcc0a2218203940dd0738b1502b75235e37
jeanm65 <70010768+jeanm65@users.noreply.github.com>
2022-07-02 15:51:48 +0300

    refactoring2

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
+import React from "react";
 
-const Alert = ({type, message}) => {
-  if()
-  return (
-    <div>
-      
-    </div>
-  );
+const Alert = ({ type, message }) => {
+  if (type === "") return;
+  return <div className={type}>{message}</div>;
 };
 
 export default Alert;
